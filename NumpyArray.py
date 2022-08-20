213f26e32ff05b816bf9a18d3ac396c2eee3c8a5
mukulverma2408 <mukulverma2408@gmail.com>
2022-08-20 21:02:07 +0530

    Added Progg 20th Aug

--- NumpyArray.py
+++ NumpyArray.py
@@ -1,10 +1,10 @@
 import numpy
 import numpy as np
 def getMax(arr):
-    print(np.max(arr))
+    print(np.min(arr))
 
 Arr = numpy.array([[1,2,3,4,5],
-                  [6,7,8,37,10],
+                  [6,7,8,-37,10],
                   [11,12,13,14,15],
                   [16,17,18,19,20]])
 
