dd8d0b81cd765e900b3449d0ea57be0043b6c662
mukulverma2408 <mukulverma2408@gmail.com>
2022-08-20 20:58:04 +0530

    Added Progg 20th Aug

---
+++ NumpyArray.py
@@ -0,0 +1,11 @@
+import numpy
+import numpy as np
+def getMax(arr):
+    print(np.max(arr))
+
+Arr = numpy.array([[1,2,3,4,5],
+                  [6,7,8,37,10],
+                  [11,12,13,14,15],
+                  [16,17,18,19,20]])
+
+getMax(Arr)
