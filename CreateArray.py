c562d0090fb5824d3fa88b9d37ce722243de1fab
mukulverma2408 <mukulverma2408@gmail.com>
2022-09-01 22:49:59 +0530

    Added Progg 01st Sep

---
+++ CreateArray.py
@@ -0,0 +1,9 @@
+#Write a NumPy program to create an array of 5 zeros,5 ones, 5 fives
+import numpy as np
+arr1 = np.zeros((2,3), int)
+arr2 = np.ones((2,3), int)
+arr3 = np.ones((2,3), int)
+arr3 = arr3*5
+print(arr1)
+print(arr2)
+print(arr3)
