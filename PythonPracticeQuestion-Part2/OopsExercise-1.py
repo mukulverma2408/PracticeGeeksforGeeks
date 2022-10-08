2498f13305629411cd92e99e45a98c87c11a407f
mukulverma2408 <mukulverma2408@gmail.com>
2022-10-08 08:54:28 +0530

    Added Progg 08th oct

--- PythonPracticeQuestion-Part2/OopsExercise-1.py
+++ PythonPracticeQuestion-Part2/OopsExercise-1.py
@@ -8,6 +8,16 @@ class Vehicle:
         return f"The seating capacity of a {self.name} is {capacity} passengers"
 
 class Bus(Vehicle):
-#    def seating_capacity(self, capacity=50):
-    pass
+    def seating_capacity(self, capacity=50):
+        return Vehicle.seating_capacity(self, capacity)
+
+""" 
+bus = Bus('School Volvo', 180, 12)
+print("Vehicle Name: {0} Speed:{1} Mileage:{2}".format(bus.name, bus.max_speed, bus.mileage))
+"""
+
+bus = Bus("Bus", 180, 12)
+car = Vehicle("car", 200, 20)
+print(bus.seating_capacity())
+print(car.seating_capacity(4))
 
---
+++ PythonPracticeQuestion-Part2/OopsExercise-3.py
@@ -0,0 +1,19 @@
+class Vehicle:
+    color = 'white'
+
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+class Bus(Vehicle):
+    pass
+
+class Car(Vehicle):
+    pass
+
+bus = Bus("School Volvo", 180, 12)
+car = Car("Audi Q5", 240, 18)
+
+print(bus.color)
+print(car.color)
