a45897a12d946fb2d2c9d909ee5822617d9a874a
mukulverma2408 <mukulverma2408@gmail.com>
2023-02-08 07:25:25 +0530

    Added Progg 08th Feb

---
+++ PythonPracticeQuestion-Part2/DataFrame-27.py
@@ -0,0 +1,20 @@
+#Conditional operation on Pandas DataFrame columns
+
+import pandas as pd
+
+# Create the dataframe
+df = pd.DataFrame({'Date': ['10/2/2011', '11/2/2011', '12/2/2011', '13/2/2011'],
+                   'Product': ['Umbrella', 'Mattress', 'Badminton', 'Shuttle'],
+                   'Last Price': [1200, 1500, 1600, 352],
+                   'Updated Price': [1250, 1450, 1550, 400],
+                   'Discount': [10, 10, 10, 10]})
+
+# Print the dataframe
+print(df)
+
+if 'Updated Price' in df.columns:
+    df['Final Price'] = (df['Updated Price'] * 90/100)
+else:
+    df['Final Price'] = (df['Last Price'] * 90 / 100)
+
+print(df)
