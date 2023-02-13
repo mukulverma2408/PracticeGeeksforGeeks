909f527797710b545f1dc121594baf5db920f63e
mukulverma2408 <mukulverma2408@gmail.com>
2023-02-13 23:00:40 +0530

    Added Progg 13th Feb

---
+++ PythonPracticeQuestion-Part2/DataFrame-33.py
@@ -0,0 +1,11 @@
+#Split a column in Pandas dataframe and get part of it
+
+import pandas as pd
+import numpy as np
+df = pd.DataFrame({'Geek_ID':['Geek1_id', 'Geek2_id', 'Geek3_id',
+                                         'Geek4_id', 'Geek5_id'],
+                'Geek_A': [1, 1, 3, 2, 4],
+                'Geek_B': [1, 2, 3, 4, 6],
+                'Geek_R': np.random.randn(5)})
+print(df)
+print(df['Geek_ID'].str.split('_').str[0])
